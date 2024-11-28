0f8493dbf1003fa9b043ee6669a292b7c6681fd3
joancuni <joancuni2@gmail.com>
2024-11-28 11:38:46 +0100

    update 01-ex00

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.com +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 14:30:05 by jocuni-p          #+#    #+#             */
-/*   Updated: 2024/11/27 17:39:37 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2024/11/28 11:33:41 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,32 @@
 
 int	main()
 {
+
+	
 	randomChump("Betsy");
 	randomChump("Jack");
 	
-	Zombie *thePtrZ = newZombie("Stormy");
+	Zombie *thePtrZ = newZombie("Stormy");//puede vivir mas alla del ambito del main.
 	thePtrZ->announce();
 	delete(thePtrZ);
-
-	
+	//delete libera e invoca automaticamente al destructor del objeto thePtr en el heap
 	return 0;
 }
+//Al salir del ambito de la funcion donde se define
+
+
+
+
+/*
+int	main()
+{
+	randomChump("Betsy");
+	randomChump("Jack");
+	
+	Zombie *thePtrZ = newZombie("Stormy");//puede vivir mas alla del ambito del main.
+	thePtrZ->announce();
+	delete(thePtrZ);
+	//delete libera e invoca automaticamente al destructor del objeto thePtr en el heap
+	return 0;
+}*/
+//Al salir del ambito de la funcion donde se define
--- cpp01/ex00/randomChump.cpp
+++ cpp01/ex00/randomChump.cpp
@@ -6,7 +6,7 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.com +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/27 12:00:00 by jocuni-p          #+#    #+#             */
-/*   Updated: 2024/11/27 16:35:04 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2024/11/28 10:45:34 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,5 @@ void	randomChump(std::string name)
 //				el zombie se anuncia
 	Zombie	Chump(name);
 	Chump.announce();
-//al salir de la funcion se deberia activar el destructor
 }
+//al salir del ambito de la funcion automaticamente es invocado el destructor de los obejtos en el stack
