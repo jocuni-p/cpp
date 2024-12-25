74a2ee41e629a9afe68e91cd95a013dcf1910c15
joancuni <joancuni2@gmail.com>
2024-12-25 23:38:10 +0100

    wip

--- cpp03/ex00/main.cpp
+++ cpp03/ex00/main.cpp
@@ -6,20 +6,40 @@
 /*   By: jocuni-p <jocuni-p@student.42barcelona.com +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 15:09:44 by jocuni-p          #+#    #+#             */
-/*   Updated: 2024/12/24 17:14:42 by jocuni-p         ###   ########.fr       */
+/*   Updated: 2024/12/25 23:08:42 by jocuni-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ClapTrap.hpp"
 
 int	main(){
+	
 	ClapTrap def;
 	ClapTrap thor("thor");
 	ClapTrap phil("phil");
-	ClapTrap ("shan");
+	
+	def = ClapTrap ("jack"); // Al no tener nombre NO la puedo usar despues de aqui y se destruye
 
+	def.printStatus();
+	
 	thor.attack("phil");
 	thor.printStatus();
+	
+	thor.takeDamage(3);
+	thor.printStatus();
+	
+	thor.attack("phil");
+	thor.beRepaired(1);
+	thor.printStatus();
+	for (int i = 0; i < 9; i++) {
+		thor.attack("phil");
+	}
+	thor.printStatus();
+	thor.takeDamage(9);
+	thor.printStatus();
 
+	phil.printStatus();
+	def.takeDamage(10);
+	
 	return 0;
 }
